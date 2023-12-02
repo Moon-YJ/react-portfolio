f87827b9c00bdd071bdcc6f63d967164b3672c02
Moon-YJ <tunjeong@gmail.com>
2023-12-03 02:24:34 +0900

    department 윗부분 콘텐츠 추가 및 css 작업

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -5,10 +5,13 @@ import './Department.scss';
 
 export default function Department() {
 	const path = useRef(process.env.PUBLIC_URL);
+	const [TopData, setTopData] = useState(null);
 	const [MemberData, setMemberData] = useState([]);
 	const [Tit, setTit] = useState('');
 	const [SubMemberData, setSubMemberData] = useState([]);
 	const [SubTit, setSubTit] = useState('');
+
+	//console.log(TopData);
 
 	const customTit = customText('combineTit');
 
@@ -16,10 +19,11 @@ export default function Department() {
 		try {
 			const data = await fetch(`${path.current}/DB/department.json`);
 			const json = await data.json();
-			setMemberData(Object.values(json)[0]);
-			setTit(Object.keys(json)[0]);
-			setSubMemberData(Object.values(json)[1]);
-			setSubTit(Object.keys(json)[1]);
+			setTopData(json.president);
+			setMemberData(Object.values(json)[1]);
+			setTit(Object.keys(json)[1]);
+			setSubMemberData(Object.values(json)[2]);
+			setSubTit(Object.keys(json)[2]);
 		} catch (err) {
 			console.log(err);
 		}
@@ -32,6 +36,29 @@ export default function Department() {
 	return (
 		<div className='Department'>
 			<Layout index={'01'} title={'Department'}>
+				{TopData && (
+					<section className='president'>
+						<div className='pic'>
+							<img src={`${path.current}/img/${TopData[0].pic}`} alt={TopData[0].name} />
+						</div>
+						<div className='quotes'>"</div>
+						<div className='con'>
+							<div className='con-top'>
+								<h2>
+									The Uprock team actively participates in our company development and ideas that we want to implement.
+									In some segments of our business, they think two or three steps ahead of us.
+								</h2>
+								<p className='name'>{TopData[0].name}</p>
+								<p className='position'>{TopData[0].position}</p>
+							</div>
+							<div className='con-btm'>
+								<p>PAST</p>
+								<span className='line'></span>
+								<p>FUTURE</p>
+							</div>
+						</div>
+					</section>
+				)}
 				<section className='team'>
 					<h1 className='tit'>{customTit(Tit)}</h1>
 					<div className='team-box'>

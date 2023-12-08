dcfde8a556bf9edb06f574d20d137c664ba927b1
Moon-YJ <tunjeong@gmail.com>
2023-12-08 12:42:51 +0900

    community 페이지 작업중

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -1,10 +1,13 @@
 import Layout from '../../common/layout/Layout';
 import './Community.scss';
+import { useRef } from 'react';
 
 export default function Community() {
+	const path = useRef(process.env.PUBLIC_URL);
+
 	return (
 		<Layout index={'04'} title={'Community'}>
-			Community
+			<img src={`${path.current}/img/community/pic-con1.png`} alt='led lamp' />
 		</Layout>
 	);
 }

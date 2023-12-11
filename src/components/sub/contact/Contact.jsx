85955dc180de378c1811d9e57c7b3d3f5f83e891
Moon-YJ <tunjeong@gmail.com>
2023-12-11 23:48:00 +0900

    contact 페이지에 kakao map 출력

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useRef } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
 
 export default function Contact() {
+	const mapFrame = useRef(null);
+	const kakao = useRef(window.kakao);
+	const mapOption = {
+		center: new kakao.current.maps.LatLng(33.450701, 126.570667),
+		level: 3,
+	};
+
+	useEffect(() => {
+		const map = new kakao.current.maps.Map(mapFrame.current, mapOption);
+	}, []);
 	return (
 		<Layout index={'06'} title={'Contact'}>
-			Members
+			<div className='map-box' ref={mapFrame}></div>
 		</Layout>
 	);
 }

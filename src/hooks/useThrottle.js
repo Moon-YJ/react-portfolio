236a1e691f699284599e9029942b21beb3f9d3ae
Moon-YJ <tunjeong@gmail.com>
2023-12-20 22:33:31 +0900

    contact 컴포넌트 로드뷰 관련 불필요한 함수 호출 방지 및 resize 핸들러 함수에 throttle 적용

---
+++ src/hooks/useThrottle.js
@@ -0,0 +1,12 @@
+import { useRef } from 'react';
+
+export const useThrottle = (func, delay = 500) => {
+	const blocker = useRef(null);
+	return () => {
+		if (blocker.current) return;
+		blocker.current = setTimeout(() => {
+			func();
+			blocker.current = null;
+		}, delay);
+	};
+};

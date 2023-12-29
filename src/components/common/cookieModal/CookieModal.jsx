5c19c9a9ed91dd6e20727a500249d91a71fe41ea
Moon-YJ <tunjeong@gmail.com>
2023-12-29 10:20:58 +0900

    Cookie팝업 css 수정

--- src/components/common/cookieModal/CookieModal.jsx
+++ src/components/common/cookieModal/CookieModal.jsx
@@ -17,34 +17,36 @@ export default function CookieModal({ wid, hgt, titTxt, infoTxt, btnTxt }) {
 	return (
 		<>
 			{!Close && (
-				<aside
-					className='CookieModal'
-					style={{ width: wid, height: hgt, marginTop: -hgt / 2, marginLeft: -wid / 2 }}>
-					<div className='content'>
-						<span>
-							<LiaCookieBiteSolid />
-						</span>
-						<h1>{titTxt}</h1>
-						<p>{infoTxt}</p>
-					</div>
-					<div className='input-set'>
-						<input
-							ref={chk}
-							type='checkbox'
-						/>
-						<button className='txt'>Don't show this message again for 24 hours.</button>
-					</div>
-					<button
-						className='confirm'
-						onClick={handleConfirm}>
-						{btnTxt}
-					</button>
-					<button
-						className='close'
-						onClick={handleConfirm}>
-						<IoMdClose />
-					</button>
-				</aside>
+				<div className='bg'>
+					<aside
+						className='CookieModal'
+						style={{ width: wid, height: hgt, marginTop: -hgt / 2, marginLeft: -wid / 2 }}>
+						<div className='content'>
+							<span>
+								<LiaCookieBiteSolid />
+							</span>
+							<h1>{titTxt}</h1>
+							<p>{infoTxt}</p>
+						</div>
+						<div className='input-set'>
+							<input
+								ref={chk}
+								type='checkbox'
+							/>
+							<button className='txt'>Don't show this message again for 24 hours.</button>
+						</div>
+						<button
+							className='confirm'
+							onClick={handleConfirm}>
+							{btnTxt}
+						</button>
+						<button
+							className='close'
+							onClick={handleConfirm}>
+							<IoMdClose />
+						</button>
+					</aside>
+				</div>
 			)}
 		</>
 	);

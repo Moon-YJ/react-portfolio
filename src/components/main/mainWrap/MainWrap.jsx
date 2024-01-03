5ce34351c2fa38683130e5c1a5458efc16506eb2
Moon-YJ <tunjeong@gmail.com>
2024-01-04 01:04:10 +0900

    메인 및 header 작업중

--- src/components/main/mainWrap/MainWrap.jsx
+++ src/components/main/mainWrap/MainWrap.jsx
@@ -1,12 +1,12 @@
-//import Vid from '../vid/Vid';
+import Vid from '../vid/Vid';
 import Visual from '../visual/Visual';
 import './MainWrap.scss';
 
 export default function MainWrap() {
 	return (
 		<div className='MainWrap'>
+			<Vid />
 			<Visual />
-			{/* <Vid /> */}
 		</div>
 	);
 }
--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -5,33 +5,41 @@ import 'swiper/css/pagination';
 import { useYoutubeQuery } from '../../../hooks/useYoutubeQuery';
 import './Visual.scss';
 //import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { customText } from '../../../hooks/useText';
 
 export default function Visual() {
 	const { data, isSuccess } = useYoutubeQuery();
+	const [Index, setIndex] = useState(0);
 	const swiperOpt = useRef({
 		modules: [Pagination, Autoplay],
 		pagination: { clickable: true },
 		autoplay: { delay: 3000 },
 		loop: true,
-		slidesPerView: 3,
+		slidesPerView: 1,
 		spaceBetween: 120,
 		centeredSlides: true,
 		allowTouchMove: false,
-		onSwiper: swiper => swiper.slideNext(300)
+		breakpoints: {
+			640: { slidesPerView: 2 },
+			1000: { slidesPerView: 3 }
+		},
+		onSwiper: swiper => swiper.slideNext(300),
+		onSlideChange: swiper => setIndex(swiper.realIndex)
 	});
 	const shortenTit = customText('shorten');
 
 	return (
 		<div className='Visual'>
 			<Swiper {...swiperOpt.current}>
+				<p className='num'>{'0' + (Index + 1)}</p>
 				{isSuccess &&
 					data.map((vid, idx) => {
 						if (idx >= 5) return null;
 						return (
 							<SwiperSlide key={vid.id}>
 								<div className='inner'>
+									{/* <h3 className='num'>{idx + 1}</h3> */}
 									<div className='pic-box'>
 										<div className='pic'>
 											<img

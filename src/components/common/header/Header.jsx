b5dc0ebbba9ea3b160653631ed8735f55a8092b6
Moon-YJ <tunjeong@gmail.com>
2024-01-05 01:15:39 +0900

    header 스크롤 감지 기능 추가

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,17 +1,50 @@
 import './Header.scss';
 import { Link, NavLink } from 'react-router-dom';
-import { useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useCommonData } from '../../../hooks/useCommonData';
 import DarkTheme from '../darkTheme/DarkTheme';
 import ColorTheme from '../colorTheme/ColorTheme';
 
 export default function Header() {
 	const path = useRef(process.env.PUBLIC_URL);
+	const wrap = useRef(null);
+	const isHide = useRef(false);
+	const isReset = useRef(true);
+	const [PosScroll, setPosScroll] = useState(0);
+	const [PosWheel, setPosWheel] = useState(100);
 	const menuEl = ['department', 'youtube', 'gallery', 'community', 'member', 'contact'];
 	const { MenuToggle, setMenuToggle } = useCommonData();
 
+	const handleScroll = useCallback(() => {
+		const deltaY = wrap.current.scrollTop - PosScroll;
+		isHide.current = wrap.current.scrollTop !== 0 && deltaY >= 0;
+		if (wrap.current.scrollTop === 0) isReset.current = true;
+		else isReset.current = false;
+		setPosScroll(wrap.current.scrollTop);
+	}, [PosScroll]);
+
+	const handleWheel = e => {
+		console.log(wrap.current.scrollTop, '::wrap');
+		console.log(wrap.current.offsetTop, 'offsetTop');
+		console.log(e.deltaY);
+		if (wrap.current.scrollTop === 0) isReset.current = true;
+		else isReset.current = false;
+		setPosWheel(wrap.current.scrollTop);
+	};
+
+	useEffect(() => {
+		isReset.current = true;
+		wrap.current = document.querySelector('.wrap');
+		wrap.current.addEventListener('scroll', handleScroll);
+		wrap.current.addEventListener('mousewheel', handleWheel);
+		return () => {
+			wrap.current.removeEventListener('scroll', handleScroll);
+			wrap.current.removeEventListener('mousewheel', handleWheel);
+		};
+	}, [handleScroll]);
+
 	return (
-		<header className='Header'>
+		<header className={`Header ${isHide.current ? 'scrolled' : ''} ${isReset.current ? 'reset' : ''}`}>
 			<h1 className='logo'>
 				<Link to='/'>
 					<img

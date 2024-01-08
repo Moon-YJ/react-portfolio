d86f1db43ed679b4d18b4a7587ed10f47f4c48cc
Moon-YJ <tunjeong@gmail.com>
2024-01-08 23:57:13 +0900

    스크롤 이벤트 핸들러 함수에 throttle 적용 및 활성화 기준점 추가

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -1,32 +1,34 @@
 import './Btns.scss';
 import { useEffect, useRef, useState } from 'react';
 import Anime from '../../../asset/anime';
+import { useThrottle } from '../../../hooks/useThrottle';
 
 export default function Btns() {
-	const [Index] = useState(0);
 	const [Num, setNum] = useState(0);
 	const wrap = useRef(null);
 	const contents = useRef(null);
 	const refBtns = useRef(null);
+	const baseLine = useRef(-window.innerHeight / 2);
 
 	const handleScroll = () => {
 		const scroll = wrap.current.scrollTop;
 		contents.current.forEach((_, idx) => {
-			if (scroll >= contents.current[idx].offsetTop) {
+			if (scroll >= contents.current[idx].offsetTop + baseLine.current) {
 				Array.from(refBtns.current.children).forEach(btn => btn.classList.remove('on'));
 				refBtns.current.children[idx].classList.add('on');
 			}
 		});
 	};
+	const throttledScroll = useThrottle(handleScroll);
 
 	useEffect(() => {
 		wrap.current = document.querySelector('.wrap');
 		contents.current = document.querySelectorAll('.scrolling');
 		setNum(contents.current.length);
 
-		wrap.current.addEventListener('scroll', handleScroll);
-		return () => wrap.current.removeEventListener('scroll', handleScroll);
-	}, []);
+		wrap.current.addEventListener('scroll', throttledScroll);
+		return () => wrap.current.removeEventListener('scroll', throttledScroll);
+	}, [throttledScroll]);
 
 	return (
 		<ul
@@ -38,7 +40,7 @@ export default function Btns() {
 					return (
 						<li
 							key={idx}
-							className={idx === Index ? 'on' : ''}
+							className={idx === 0 ? 'on' : ''}
 							onClick={() => {
 								new Anime(wrap.current, { scroll: contents.current[idx].offsetTop }, { duration: 500 });
 							}}></li>

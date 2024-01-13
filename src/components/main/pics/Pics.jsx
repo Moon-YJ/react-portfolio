286421b928db978259f38ea3af73579299da0a9b
Moon-YJ <tunjeong@gmail.com>
2024-01-14 02:42:42 +0900

    Text 컴포넌트 애니메이션 수정

--- src/components/main/pics/Pics.jsx
+++ src/components/main/pics/Pics.jsx
@@ -2,26 +2,44 @@ import './Pics.scss';
 import { useYoutubeQuery } from '../../../hooks/useYoutubeQuery';
 import { useCommonData } from '../../../hooks/useCommonData';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { customText } from '../../../hooks/useText';
 import { IoPlayCircleOutline } from 'react-icons/io5';
 import Modal from '../../common/modal/Modal';
+import { useScroll } from '../../../hooks/useScroll';
 
 export default function Pics() {
 	const { data: Vids, isSuccess } = useYoutubeQuery();
 	const { setOpen } = useCommonData();
 	const [Index, setIndex] = useState(0);
+	const { getScrollPos, refTarget, Frame } = useScroll();
 	const shortenTit = customText('shorten');
 	const customDate = customText('combine');
 
 	const handleModal = idx => {
-		setOpen(true);
 		setIndex(idx);
+		setOpen(true);
 	};
+
+	const handleScroll = useCallback(() => {
+		const scroll = getScrollPos(-window.innerHeight / 3);
+		if (scroll >= 0) {
+			refTarget.current.classList.add('on');
+		} else {
+			refTarget.current.classList.remove('on');
+		}
+	}, [getScrollPos, refTarget]);
+
+	useEffect(() => {
+		Frame?.addEventListener('scroll', handleScroll);
+		return () => Frame?.removeEventListener('scroll', handleScroll);
+	}, [Frame, handleScroll]);
 
 	return (
 		<>
-			<section className='Pics scrolling'>
+			<section
+				className='Pics scrolling'
+				ref={refTarget}>
 				<div className='tit-set'>
 					<h1 className='tit'>Latest Contents</h1>
 					<div className='detail'>
--- src/components/main/text/Text.jsx
+++ src/components/main/text/Text.jsx
@@ -7,18 +7,20 @@ export default function Text() {
 	const txtRef2 = useRef(null);
 	const { getScrollPos, refTarget, Frame } = useScroll();
 	const handleScroll = useCallback(() => {
-		const scroll = getScrollPos();
+		const scroll = getScrollPos(50);
 		if (txtRef.current && txtRef2.current) {
 			if (scroll >= 0) {
-				txtRef.current.style.transform = `translateX(${scroll / 2.5}px)`;
-				txtRef2.current.style.transform = `translateX(${-scroll / 5}px)`;
-				txtRef.current.style.opacity = 0.3 + scroll / 600;
-				txtRef2.current.style.opacity = 0.3 + scroll / 800;
+				txtRef.current.style.transform = `translateX(${-scroll / 3}px) scale(${1 + scroll / 1500})`;
+				txtRef2.current.style.transform = `translateX(${scroll / 3}px) translateY(${scroll / 3}px) scale(${
+					1 + scroll / 1500
+				})`;
+				txtRef.current.style.opacity = 0.4 - scroll / 1500;
+				txtRef2.current.style.opacity = 0.4 - scroll / 1500;
 			} else {
 				txtRef.current.style.transform = `translateX(0)`;
 				txtRef2.current.style.transform = `translateX(0)`;
-				txtRef.current.style.opacity = 0.4;
-				txtRef2.current.style.opacity = 0.4;
+				txtRef.current.style.opacity = 0.2;
+				txtRef2.current.style.opacity = 0.2;
 			}
 		}
 	}, [getScrollPos]);
